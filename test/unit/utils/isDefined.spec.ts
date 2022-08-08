86eccd8d8fa8473e1b3186b8c381431cf2733eda
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2022-08-09 00:19:59 +0200

    test(object-observable): add tests to object observable class and isDefined helper
    Related work item: GH-20

---
+++ test/unit/utils/isDefined.spec.ts
@@ -0,0 +1,76 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { isDefined } from '../../../src/utils/isDefined';
+
+describe(isDefined.name, () => {
+  describe('Truthy values', () => {
+    it(`should return true if true`, () => {
+      expect(isDefined(true)).to.be.true;
+    });
+    it(`should return true if positive number`, () => {
+      expect(isDefined(1)).to.be.true;
+    });
+    it(`should return true if positive BigInt`, () => {
+      expect(isDefined(1n)).to.be.true;
+    });
+    it(`should return true if negative BigInt`, () => {
+      expect(isDefined(-1n)).to.be.true;
+    });
+    it(`should return true if Infinity`, () => {
+      expect(isDefined(Infinity)).to.be.true;
+    });
+    it(`should return true if -Infinity`, () => {
+      expect(isDefined(-Infinity)).to.be.true;
+    });
+    it(`should return true if negative number`, () => {
+      expect(isDefined(-1)).to.be.true;
+    });
+    it(`should return true if 'false' string`, () => {
+      expect(isDefined('false')).to.be.true;
+    });
+    it(`should return true if '0' string`, () => {
+      expect(isDefined('0')).to.be.true;
+    });
+    it(`should return true if any string longer than 0`, () => {
+      expect(isDefined('.')).to.be.true;
+    });
+    it(`should return true if empty object {}`, () => {
+      expect(isDefined({})).to.be.true;
+    });
+    it(`should return true if empty array []`, () => {
+      expect(isDefined([])).to.be.true;
+    });
+    it(`should return true if Date object`, () => {
+      expect(isDefined(new Date())).to.be.true;
+    });
+    it(`should return true if false`, () => {
+      expect(isDefined(false)).to.be.true;
+    });
+    it(`should return true if 0`, () => {
+      expect(isDefined(0)).to.be.true;
+    });
+    it(`should return true if -0`, () => {
+      expect(isDefined(-0)).to.be.true;
+    });
+    it(`should return true if BigInt 0`, () => {
+      expect(isDefined(0n)).to.be.true;
+    });
+    it(`should return true if negative BigInt 0`, () => {
+      expect(isDefined(-0n)).to.be.true;
+    });
+    it(`should return true if empty string`, () => {
+      expect(isDefined('')).to.be.true;
+    });
+    it(`should return true if NaN`, () => {
+      expect(isDefined(NaN)).to.be.true;
+    });
+  });
+  describe('Falsy values', () => {
+    it(`should return false if null`, () => {
+      expect(isDefined(null)).to.be.false;
+    });
+    it(`should return false if undefined`, () => {
+      expect(isDefined(undefined)).to.be.false;
+    });
+  });
+});
