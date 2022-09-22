73eba99a685f34212fa2f10d4d386a91ad64c553
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2022-09-22 23:50:04 +0200

    chore(js-dom): add js-dom and setup navigator mock
    Related work item: GH-26

--- test/mochaSetup.ts
+++ test/mochaSetup.ts
@@ -1,4 +1,11 @@
 import * as chai from 'chai';
+import { NAVIGATOR } from './mocks/NAVIGATOR.mock';
 import sinonChai from 'sinon-chai';
 
 chai.use(sinonChai);
+
+Object.defineProperty(window, 'navigator', {
+  get: () => {
+    return NAVIGATOR;
+  },
+});
